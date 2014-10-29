d7ab2c115ab36ba9b34c242698382b43ef3b71fb
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-10-29 03:46:38 -0600

    added title

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+var s = skrollr.init({
+	smoothScrolling: false
+});
 
+if(s.isMobile()){
+	s.destroy()
+}
+
+$(function () {
+	$("#title h1, #title h2").bigtext({
+	    childSelector: '> span'
+	});
+});
