63608d104e778cf65b9ddc018c9c5c03a18771c4
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-10-31 05:32:41 -0600

    stability improvements - try/catch + checking

--- js/main.js
+++ js/main.js
@@ -6,24 +6,30 @@ var s = skrollr.init({
 		if(typeof isMobile === "undefined" && typeof s !== "undefined") {
 			isMobile = s.isMobile();
 		}
-		if(!isMobile && typeof s !== "undefined") {
-			var indicator = document.getElementById("indicator");
-			if(indicator.innerText.replace(",", "") != indicator.getAttribute("count")) {
-				indicator.innerText = Math.round(indicator.getAttribute("count")).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-			}
-		}
-		var graph = document.getElementById("graph");
-		if(boxes != graph.getAttribute("boxes")) {
-			boxes = graph.getAttribute("boxes");
-			var intBoxes = Math.floor(boxes);
-			var trNumber = (intBoxes % 7 || 7) - 1;
-			var tdNumber = Math.floor(boxes / 7);
 
-			var index = Math.round(Math.random() * 9);
-			if(typeof $ !== "undefined") {
-				$(graph).find("tr").eq(trNumber).find("td").eq(tdNumber).css("background", colors[index]);
-			}
+		if(!isMobile && typeof s !== "undefined") {
+			try {
+				var indicator = document.getElementById("indicator");
+				if(typeof indicator.innerText !== "undefined" && indicator.innerText.replace(",", "") != indicator.getAttribute("count")) {
+					indicator.innerText = Math.round(indicator.getAttribute("count")).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+				}
+			} catch (e) { }
 		}
+
+		var graph = document.getElementById("graph");
+		try {
+			if(boxes != graph.getAttribute("boxes")) {
+				boxes = graph.getAttribute("boxes");
+				var intBoxes = Math.floor(boxes);
+				var trNumber = (intBoxes % 7 || 7) - 1;
+				var tdNumber = Math.floor(boxes / 7);
+
+				var index = Math.round(Math.random() * 9);
+				if(typeof $ !== "undefined") {
+					$(graph).find("tr").eq(trNumber).find("td").eq(tdNumber).css("background", colors[index]);
+				}
+			}
+		} catch (e) { }
 	}
 });
 
